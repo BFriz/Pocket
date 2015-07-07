60e02330e7f12c836fbf01077421c63deb965078
Ben Frisby <bfrisb@gmail.com>
2015-07-07 19:17:13 +0100

    making sure my assocaitions are right - have added a join table for user_articles

--- database/db/schema.rb
+++ database/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707172343) do
+ActiveRecord::Schema.define(version: 20150707175612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150707172343) do
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_articles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
