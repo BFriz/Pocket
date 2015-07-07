a5f3131fe60991c06de37224d3224d53254840d6
Ben Frisby <bfrisb@gmail.com>
2015-07-07 20:10:27 +0100

    removed server (again) and added url to my articles model

--- database/db/schema.rb
+++ database/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707175612) do
+ActiveRecord::Schema.define(version: 20150707190925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20150707175612) do
     t.string   "title"
     t.string   "author"
     t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "url"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.string   "content"
+    t.string   "user_id"
+    t.string   "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
