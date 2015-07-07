f53f676b1311e96dfda062ee83bf70fa17009c7f
Ben Frisby <bfrisb@gmail.com>
2015-07-07 18:41:17 +0100

    updated article model and seeded some data

--- database/db/seeds.rb
+++ database/db/seeds.rb
@@ -6,9 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
+Article.delete_all
+
 u1 = User.create(name: 'Ben', email: 'bfrisb@gmail.com')
 u2 = User.create(name: 'Laura', email: 'Laura@gmail.com')
 u3 = User.create(name: 'Mathilda', email: 'Mathilda@gmail.com')
+
+a1 = Article.create(title: 'Adventures of a teenage girl', author: 'Dr. B Frisby', category: 'Motivational')
+a2 = Article.create(title: 'Confessions of a Merkaat', author: 'Ce Moorebuts', category: 'Hip')
+a3 = Article.create(title: 'The Diary of the Jamican Ski Team', author: 'Dick Hertz', category: 'Uplifting')
+
 
 u1.friendships.create(friend_id: u2.id)
 u1.friendships.create(friend_id: u3.id)
