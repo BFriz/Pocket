f0532ce0da21c6cc7e74e6c5864f0778e3f85f93
Ben Frisby <bfrisb@gmail.com>
2015-08-02 14:49:48 +0100

    still can't figure out how to grab the current_user in my articles controller so I fixed a few of the routing issues with logged in and logged out

--- database/app/controllers/articles_controller.rb
+++ database/app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   # skip_before_filter :require_no_authentication, only: [:create]
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
 
   def index
     articles = Article.all.order(:id)
@@ -9,15 +9,16 @@ class ArticlesController < ApplicationController
     render :json => articles, :include => :categories
   end
   def create
+    # binding.pry
 
     article = Article.create(url: params[:url])
     article.scrapedata
+    
 
     params[:categories].split(', ').each do |name|
       category = Category.create(name: name)
       ArticleCategory.create(category_id: category.id, article_id: article.id)
     end
-    binding.pry
 
     UserArticle.create(user_id: current_user, article_id: article.id)
 
@@ -30,6 +31,7 @@ class ArticlesController < ApplicationController
   end
 
   def user_articles
+
     user = User.find(params[:user]) 
     if user
       render :json => user.articles, :include => :categories
